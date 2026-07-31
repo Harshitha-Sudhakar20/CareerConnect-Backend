2688ade6111a74a98399109c5681773c90ba74a2
Harsh <harshithashettyh34@gmail.com>
2026-07-31 12:34:10 +0530

    Fix Railway HTTPS proxy

--- src/main/java/com/harshitha/careerconnect/config/OpenApiConfig.java
+++ src/main/java/com/harshitha/careerconnect/config/OpenApiConfig.java
@@ -1,7 +1,7 @@
 package com.harshitha.careerconnect.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -14,20 +14,20 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
 
-        final String securitySchemeName = "bearerAuth";
+        String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
                 .info(new Info()
                         .title("CareerConnect API")
                         .version("1.0"))
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
+                        .addSecuritySchemes(
+                                securitySchemeName,
                                 new SecurityScheme()
-                                        .name(securitySchemeName)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")
+                        ));
     }
 }
